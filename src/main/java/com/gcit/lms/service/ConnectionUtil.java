f3399d402bd522c788d31d8b35ddb39a9f5a2daa
Fayez Ahmed <fayahm3@gmail.com>
2017-07-14 15:51:38 -0400

    changes connection url for RDS data base

--- src/main/java/com/gcit/lms/service/ConnectionUtil.java
+++ src/main/java/com/gcit/lms/service/ConnectionUtil.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class ConnectionUtil {
 
 	public String driver = "com.mysql.cj.jdbc.Driver";
-	public String url = "jdbc:mysql://localhost/library?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	public String username = "root";
-	public String password = "root";
+	public String url = "jdbc:mysql://gcitsampledb.cbhdojzwpgzp.us-east-2.rds.amazonaws.com:3306/library?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	public String username = "gcitsampledb";
+	public String password = "gcitsampledb";
 
 	public Connection getConnection() {
 		Connection conn = null;
